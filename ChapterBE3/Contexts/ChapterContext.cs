e7592d09864146d7cee4851034ff5b9be414ffe4
Julio Polistschuk <juliopolistschukw@gmail.com>
2022-06-12 17:04:55 -0300

    ..

---
+++ ChapterBE3/Contexts/ChapterContext.cs
@@ -0,0 +1,30 @@
+﻿using ChapterBE3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterBE3.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public  ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        //vamos utilizar esse método para configurar o banco de dados
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-P894BOF\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+                //em vez de user id/pass, pode se usar caso de erro em /api/livro Integrated Security = true");, User Id = sa; Password = 1"
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
